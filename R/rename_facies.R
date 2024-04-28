82609b3db6215f9c3a3ca335b45527020b21d76a
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2024-04-28 17:01:14 +0200

    add functionality

---
+++ R/rename_facies.R
@@ -0,0 +1,24 @@
+rename_facies = function(s, new_names, old_names = NULL){
+  #' @title rename facies
+  #'
+  #' @param s stratcol object
+  #' @param new_names new facies names
+  #' @param old_names NULL or a list of old facies names. If NULL, all old facies names will be used
+  #'
+  #' @description
+    #' replaces old facies names with new ones
+    #'
+  #' @returns a stratcol object
+  #'
+
+  if (is.null(old_names)){
+    old_names = unique(s$fa)
+  }
+
+  new_fa = rep(NA, length(s$fa))
+  for ( i in seq_along(old_names)){
+    new_fa = replace(new_fa, s$fa == old_names[i], new_names[i])
+  }
+  s$fa = new_fa
+  return(s)
+}
