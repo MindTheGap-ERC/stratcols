a36d8ba75e33d6c35c04db4592ed528650dc651f
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2024-04-30 09:29:23 +0200

    fix namespace for tests

--- tests/testthat/test_get_rom.R
+++ tests/testthat/test_get_rom.R
@@ -1,10 +1,10 @@
 test_that("results match for tickening and thinning sections", {
   no_beds = 11
   no_transitions = no_beds - 1
-  s = as_stratcol(thickness = seq_len(no_beds), facies = seq_len(no_beds))
+  s = stratcols::as_stratcol(thickness = seq_len(no_beds), facies = seq_len(no_beds))
   expect_equal(get_rom(s), sum(seq_len(no_transitions))/(no_transitions))
 
-  s = as_stratcol(thickness = rev(seq_len(no_beds)), facies = seq_len(no_beds))
+  s = stratcols::as_stratcol(thickness = rev(seq_len(no_beds)), facies = seq_len(no_beds))
   expect_equal(get_rom(s), sum(seq_len(no_transitions))/(no_transitions))
 
 })
@@ -12,7 +12,7 @@ test_that("results match for tickening and thinning sections", {
 test_that("results match for alternating bed thicknesses", {
   no_beds = 11
   no_transitions = no_beds - 1
-  s = as_stratcol(thickness = rep(c(1,2), no_beds)[seq_len(no_beds)],
+  s = stratcols::as_stratcol(thickness = rep(c(1,2), no_beds)[seq_len(no_beds)],
                   facies = rep(1, no_beds))
   expect_equal(get_rom(s), 1)
 })
