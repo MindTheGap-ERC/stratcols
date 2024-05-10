970e3b120dc1a2984dff4cc9c720ee3e351f96b4
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2024-05-10 09:51:15 +0200

    print summary to console

---
+++ R/summary.stratcol.R
@@ -0,0 +1,17 @@
+summary.stratcol = function(object, ...){
+  #' @export
+  #'
+  #' @title summary of stratigraphic column
+  #'
+  #' @param object stratigraphic column
+  #' @param ... further parameters (ignored)
+  #'
+  #' @returns invisible NULL. prints to the console
+  #'
+
+  cat("Stratigraphic column \n")
+  cat(paste0("No of beds: ", no_beds.stratcol(object), "\n"))
+  cat(paste0("No of facies: ", no_facies.stratcol(object), "\n"))
+  cat(paste0("Total thickness: ", total_thickness.stratcol(object), " ", get_L_unit(object), "\n"))
+
+}
