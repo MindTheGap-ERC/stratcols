20e2d7cd026c31d83349051f16a2bc02917c2523
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2024-05-10 09:50:58 +0200

    print to console

---
+++ R/print.stratcol.R
@@ -0,0 +1,14 @@
+print.stratcol = function(x, ...){
+  #' @export
+  #'
+  #' @title print stratigraphic column to console
+  #'
+  #' @param x stratigraphic column
+  #' @param ... other parameters (ignored)
+  #'
+  #' @returns invisible NULL, prints to the console
+  #'
+  #' @seealso [summary.stratcol()] for a summary of a stratigraphic column
+  cat("Stratigraphic column\n")
+  return(invisible())
+}
