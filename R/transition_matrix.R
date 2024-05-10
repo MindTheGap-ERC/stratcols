3b9d97090d87cdcadccdf1fef3fb7baebff6a8b2
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2024-05-10 10:51:11 +0200

    abstract structure, adjust class name

--- R/transition_matrix.R
+++ R/transition_matrix.R
@@ -16,17 +16,18 @@ transition_matrix.stratcol = function(s){
   n = stratcols::no_facies(s)
   m = matrix(0, ncol = n, nrow = n)
   fa = stratcols::unique_facies_names(s)
+  fa_names = stratcols::facies_names(s)
   no_bed = stratcols::no_beds(s)
   dimnames(m) = list("to" = fa, "from" = fa)
   for (l in seq_len(no_bed-1)){
-    i = s$fa[l]
-    j = s$fa[l+1]
+    i = fa_names[l]
+    j = fa_names[l+1]
     m[j,i] = m[ j, i] + 1
   }
   for (k in seq_len(n)){
     m[k,] = m[k,] /sum(m[k,])
   }
-  class(m) = "fa_tran_mat"
+  class(m) = "fa_tran_mat_p"
   return(m)
 
 }
